e21fe918a422262855b43351445f4cc57c32da78
pmcmahon14 <pmcmahon14@gmail.com>
2023-12-16 19:56:12 -0800

    calculator complete

--- divisibility.js
+++ divisibility.js
@@ -1,1 +1,80 @@
-document.addEventListener()
+const inputDisplay = document.getElementById("input");
+const answerDisplay = document.getElementById("answer");
+
+console.log(inputDisplay.value);
+
+let result = "";
+let count = 0;
+
+function appendToDisplay(input) {
+    // console.log(input);
+    // console.log((inputDisplay.value).toLocaleString());
+    if (inputDisplay.value.length === 15) {
+        //nothing
+    } else if (input === "0" && inputDisplay.value === "0") {
+        //nothing
+    } else if (inputDisplay.value === "0") {
+        console.log('display zero');
+        inputDisplay.value = input;
+    } else {
+        console.log('display');
+        let tempValue = parseInt(inputDisplay.value.replace(/,/g,""));
+        tempValue = tempValue.toString();
+        tempValue+=input;
+        tempValue = parseInt(tempValue).toLocaleString();
+        inputDisplay.value=tempValue;
+    }
+}
+
+function clearAll() {
+    inputDisplay.value ='0';
+    console.log(inputDisplay.value);
+    answerDisplay.value = "";
+}
+
+function clearError() {
+    let tempValue = parseInt(inputDisplay.value.replace(/,/g,""));
+    tempValue = Math.floor(tempValue/10);
+    if(tempValue.length === ""){
+        inputDisplay.value = "0";
+    } else {
+        tempValue = tempValue.toLocaleString();
+        inputDisplay.value=tempValue;
+    }
+}
+
+function calculate() {
+    let tempValue = parseInt(inputDisplay.value.replace(/,/g,""));
+    let divisor = 2;
+    let dividend = tempValue;
+    let divisorList = [];
+    while (divisor < 101) {
+        if (dividend % divisor === 0) {
+            divisorList.push(divisor);
+        }
+        divisor++;
+    }
+    answerDisplay.value = `${inputDisplay.value} is divisible by `;
+    count = divisorList.length;
+    let i = 0;
+    while (i < count-1) {
+        divisorList.push(i);
+        i++;
+    }
+    if (count === 0) {
+        answerDisplay.value = `${inputDisplay.value} is not divisible by any numbers between 2 and 100.`;
+    } else if (count === 1) {
+        answerDisplay.value = `${inputDisplay.value} is divisible by ${divisorList[0]}.`;
+    } else if (count === 2) {
+        answerDisplay.value = `${inputDisplay.value} is divisible by ${divisorList[0]} and ${divisorList[1]}.`;
+    } else {
+        answerDisplay.value = `${inputDisplay.value} is divisible by ${divisorList[0]}, `;
+        let j = 1;
+        while (j < count-1) {
+            answerDisplay.value = `${answerDisplay.value}${divisorList[j]}, `;
+            j++;
+        }
+        answerDisplay.value = `${answerDisplay.value}and ${divisorList[count-1]}.`;
+    }
+    console.log(answerDisplay.value);
+}
