fedbd7b6a5e5d81655b3973eb3f3152944daad80
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-21 22:08:06 -0800

    electrical conductivity finished

--- unit_conversions/Electrical Conductivity/electrical_conductivity.js
+++ unit_conversions/Electrical Conductivity/electrical_conductivity.js
@@ -1,4 +1,4 @@
-const List<String> elecCyArray = ['Ampere/Volt/Kilometer(A/V/km)', 'Ampere/Volt/Meter(A/V/m)',
+const elecCyArray = ['Ampere/Volt/Kilometer(A/V/km)', 'Ampere/Volt/Meter(A/V/m)',
   'Ampere/Volt/Centimeter(A/V/cm)', 'Ampere/Volt/Millimeter(A/V/mm)', 'Ampere/Volt/Micrometer(A/V/µm)',
   'Ampere/Volt/Nanometer(A/V/nm)', 'Ampere/Volt/Yard(A/V/yd)', 'Ampere/Volt/Foot(A/V/ft)',
   'Ampere/Volt/Inch(A/V/in)', 'Kilosiemens/Kilometer(kS/km)', 'Kilosiemens/Meter(kS/m)',
@@ -17,13 +17,24 @@ const List<String> elecCyArray = ['Ampere/Volt/Kilometer(A/V/km)', 'Ampere/Volt/
   'Siemens/Millimeter(S/mm)', 'Siemens/Micrometer(S/µm)', 'Siemens/Nanometer(S/nm)', 'Siemens/Yard(S/yd)',
   'Siemens/Foot(S/ft)', 'Siemens/Inch(S/in)'];
 
-String elecCyInitValue1 = 'Siemens/Meter(S/m)';
-String elecCyInitValue2 = 'Siemens/Kilometer(S/km)';
+const elecCyInitValue1 = "46";
+const elecCyInitValue2 = "45";
 
 //based on 1 siemens/meter
-const List<double> elecCyConvArray = [1000, 1, 0.01, 0.001, 0.000001, 1e-9, 0.9144, 0.3048, 0.0254,
+const elecCyConvArray = [1000, 1, 0.01, 0.001, 0.000001, 1e-9, 0.9144, 0.3048, 0.0254,
   1, 0.001, 0.00001, 0.000001, 1e-9, 1e-12, 0.0009144, 0.0003048, 0.0000254,
   0.001, 0.000001, 1e-8, 1e-9, 1e-12, 1e-15, 9.144e-7, 3.048e-7, 2.54e-8,
   1e9, 1000000, 10000, 1000, 1, 0.001, 914400, 304800, 25400,
   1000000, 1000, 10, 1, 0.001, 0.000001, 914.4, 304.8, 25.4,
   1000, 1, 0.01, 0.001, 0.000001, 1e-9, 0.9144, 0.3048, 0.0254];
+
+document.addEventListener("DOMContentLoaded", function(){
+  window.convArray = elecCyConvArray;
+})
+
+function clearAngle() {
+  selectedUnit1.value = elecCyInitValue1;
+  selectedUnit2.value = elecCyInitValue2;
+  originalUnit1 = elecCyInitValue1;
+  originalUnit2 = elecCyInitValue2;
+}
