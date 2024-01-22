b496605de3e6bd447e6ff28f8f4b072218ba825a
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-21 23:34:59 -0800

    enzyme finished

--- unit_conversions/Enzyme/enzyme.js
+++ unit_conversions/Enzyme/enzyme.js
@@ -1,4 +1,4 @@
-const List<String> enzymeArray = ['Yottakatal (Ykat)', 'Zettakatal (Zkat)', 'Exakatal (Ekat)',
+const enzymeArray = ['Yottakatal (Ykat)', 'Zettakatal (Zkat)', 'Exakatal (Ekat)',
   'Petakatal (Pkat)', 'Terakatal (Tkat)', 'Gigakatal (Gkat)', 'Megakatal (Mkat)', 'Kilokatal (kkat)',
   'Hectokatal (hkat)', 'Dekakatal (dkkat)', 'Katal (kat)', 'Decikatal (dkat)', 'Centikatal (ckat)',
   'Millikatal (mkat)', 'Microkatal (µkat)', 'Nanokatal (nkat)', 'Picokatal (pkat)', 'Femtokatal (fkat)',
@@ -7,9 +7,20 @@ const List<String> enzymeArray = ['Yottakatal (Ykat)', 'Zettakatal (Zkat)', 'Exa
   'Micromole/Minute (µmol/min)', 'Micromole/Second (µmol/s)', 'Mole/Minute (mol/min)',
   'Mole/Second (mol/s)'];
 
-String enzymeInitValue1 = 'Katal (kat)';
-String enzymeInitValue2 = 'Mole/Minute (mol/min)';
+const enzymeInitValue1 = "10";
+const enzymeInitValue2 = "27";
 
 //based on 1 katal
-const List<double> enzymeConvArray = [1e-24, 1e-21, 1e-18, 1e-15, 1e-12, 1e-9, 1e-6, 1000, 100, 10, 1, 0.1,
+const enzymeConvArray = [1e-24, 1e-21, 1e-18, 1e-15, 1e-12, 1e-9, 1e-6, 1000, 100, 10, 1, 0.1,
   0.01, 0.001, 1e6, 1e9, 1e12, 1e15, 1e18, 1e21, 124, 0.06, 0.001, 60000, 1000, 60000000, 1000000, 60, 1];
+
+document.addEventListener("DOMContentLoaded", function(){
+  window.convArray = enzymeConvArray;
+})
+
+function clearAngle() {
+  selectedUnit1.value = enzymeInitValue1;
+  selectedUnit2.value = enzymeInitValue2;
+  originalUnit1 = enzymeInitValue1;
+  originalUnit2 = enzymeInitValue2;
+}
