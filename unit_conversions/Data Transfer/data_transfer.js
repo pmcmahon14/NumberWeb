8034696cfe2f59e9ca3d07167ece21ec2974e7a6
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-21 20:11:24 -0800

    data transfer finished

--- unit_conversions/Data Transfer/data_transfer.js
+++ unit_conversions/Data Transfer/data_transfer.js
@@ -1,4 +1,4 @@
-const List<String> dataTransferArray = ['Bits/Day(b/d)', 'Bits/Hour(b/hr)', 'Bits/Microsecond(b/µs)',
+const dataTransferArray = ['Bits/Day(b/d)', 'Bits/Hour(b/hr)', 'Bits/Microsecond(b/µs)',
   'Bits/Millisecond(b/ms)', 'Bits/Minute(b/min)', 'Bits/Nanosecond(b/ns)', 'Bits/Second(b/s)', 'Bytes/Day(B/d)',
   'Bytes/Hour(B/hr)', 'Bytes/Microsecond(B/µs)', 'Bytes/Millisecond(B/ms)', 'Bytes/Minute(B/min)',
   'Bytes/Nanosecond(B/ns)', 'Bytes/Second(B/s)', 'Ethernet', 'Fast Ethernet', 'Gigabit Ethernet', 'Gigabits/Day(Gb/d)',
@@ -19,13 +19,24 @@ const List<String> dataTransferArray = ['Bits/Day(b/d)', 'Bits/Hour(b/hr)', 'Bit
   'Terabytes/Microsecond(TB/µs)', 'Terabytes/Millisecond(TB/ms)', 'Terabytes/Minute(TB/min)',
   'Terabytes/Nanosecond(TB/ns)', 'Terabytes/Second(TB/s)'];
 
-String dataTransferInitValue1 = 'Bytes/Second(B/s)';
-String dataTransferInitValue2 = 'Bits/Second(b/s)';
+const dataTransferInitValue1 = "13";
+const dataTransferInitValue2 = "6";
 
 //based on 1 byte/second
-const List<double> dataTransferConvArray = [691200, 28800, 0.000008, 0.008, 480, 8e-9, 8, 86400, 3600, 0.000001,
+const dataTransferConvArray = [691200, 28800, 0.000008, 0.008, 480, 8e-9, 8, 86400, 3600, 0.000001,
   0.001, 60, 1e-9, 1, 8e-7, 8e-8, 8e-9, 0.0006912, 0.0000288, 8e-15, 8e-12, 4.8e-7, 8e-18, 8e-9, 0.0000864, 0.0000036,
   1e-15, 1e-12, 6e-8, 1e-18, 1e-9, 691.2, 28.8, 8e-9, 0.000008, 0.48, 8e-12, 0.008, 86.4, 3.6, 1e-9, 0.000001,
   0.06, 1e-12, 0.001, 0.6912, 0.0288, 8e-12, 8e-9, 0.00048, 8e-15, 0.000008, 0.0864, 0.0036, 1e-12, 1e-9, 0.00006,
   1e-15, 0.000001, 6.912e-7, 2.88e-8, 8e-18, 8e-15, 4.8e-10, 8e-21, 8e-12, 8.64e-8, 3.6e-9, 1e-18, 1e-15, 6e-11, 1e-21,
   1e-12];
+
+document.addEventListener("DOMContentLoaded", function(){
+  window.convArray = dataTransferConvArray;
+})
+
+function clearAngle() {
+  selectedUnit1.value = dataTransferInitValue1;
+  selectedUnit2.value = dataTransferInitValue2;
+  originalUnit1 = dataTransferInitValue1;
+  originalUnit2 = dataTransferInitValue2;
+}
