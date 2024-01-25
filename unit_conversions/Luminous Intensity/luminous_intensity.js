9f7ff6d3155a2779c5c1f7268a4e3655a8717375
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-25 12:12:38 -0800

    luminous intensity finished

---
+++ unit_conversions/Luminous Intensity/luminous_intensity.js
@@ -0,0 +1,41 @@
+const lumIntArray = ['Bougie Decimal (bougie)', 'Candela (cd)', 'Candle (c)',
+  'Candle (Germany) (c)', 'Candle (UK) (c)', 'Carcel Unit (carcel)', 'Decimal Candle (dc)',
+  'Hefner Candle (Hc)', 'Lumen/Steradian (lm/sr)', 'Pentane Candle (pc)', 'Pentane Candle (Power 10) (pc)'];
+
+const lumIntInitValue1 = "1";
+const lumIntInitValue2 = "2";
+
+//based on 1 candela
+const lumIntConvArray = [1, 1, 1, 0.95, 0.960006144, 0.1040582726, 1, 1.111111111, 1, 1, 0.1];
+
+window.convArray = lumIntConvArray;
+
+function clearAngle() {
+  selectedUnit1.value = lumIntInitValue1;
+  selectedUnit2.value = lumIntInitValue2;
+  originalUnit1 = lumIntInitValue1;
+  originalUnit2 = lumIntInitValue2;
+}
+
+let option = "";
+for(let i=0;i<lumIntArray.length;i++){
+  if (i === parseInt(lumIntInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + lumIntArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + lumIntArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<lumIntArray.length;j++){
+  if (j === parseInt(lumIntInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + lumIntArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + lumIntArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
--- unit_conversions/luminous_intensity.dart
+++
@@ -1,9 +0,0 @@
-const List<String> lumIntArray = ['Bougie Decimal (bougie)', 'Candela (cd)', 'Candle (c)',
-  'Candle (Germany) (c)', 'Candle (UK) (c)', 'Carcel Unit (carcel)', 'Decimal Candle (dc)',
-  'Hefner Candle (Hc)', 'Lumen/Steradian (lm/sr)', 'Pentane Candle (pc)', 'Pentane Candle (Power 10) (pc)'];
-
-String lumIntInitValue1 = 'Candela (cd)';
-String lumIntInitValue2 = 'Candle (c)';
-
-//based on 1 candela
-const List<double> lumIntConvArray = [1, 1, 1, 0.95, 0.960006144, 0.1040582726, 1, 1.111111111, 1, 1, 0.1];
