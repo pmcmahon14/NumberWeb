e385a21167a341d888b420230425a918d7749608
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-24 16:17:43 -0800

    added dynamic dropdowns to dna length

--- unit_conversions/DNA Length/dna_length.js
+++ unit_conversions/DNA Length/dna_length.js
@@ -17,3 +17,26 @@ function clearAngle() {
   originalUnit1 = dnaLengthInitValue1;
   originalUnit2 = dnaLengthInitValue2;
 }
+
+let option = "";
+for(let i=0;i<dnaLengthArray.length;i++){
+  if (i === parseInt(dnaLengthInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + dnaLengthArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + dnaLengthArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<dnaLengthArray.length;j++){
+  if (j === parseInt(dnaLengthInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + dnaLengthArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + dnaLengthArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
