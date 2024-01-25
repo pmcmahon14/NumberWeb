333224d73dcb2cf627d190febe159db6638dbdd6
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-24 19:30:46 -0800

    added dynamic dropdowns to electrical potential

--- unit_conversions/Electrical Potential/electrical_potential.js
+++ unit_conversions/Electrical Potential/electrical_potential.js
@@ -3,8 +3,8 @@ const elecPotentialArray = ['Abvolt (abV)', 'EMU of Electric Potential (EMU)',
   'Millivolt (mV)', 'Nanovolt (nV)', 'Picovolt (pV)', 'Planck Voltage (hV)', 'Statvolt (stV)', 'Teravolt (TV)',
   'Volt (V)', 'Watt/Ampere (W/A)'];
 
-const elecPotentialInitValue1 = 'Volt (V)';
-const elecPotentialInitValue2 = 'Watt/Ampere (W/A)';
+const elecPotentialInitValue1 = "13";
+const elecPotentialInitValue2 = "14";
 
 //based on 1 volt
 const elecPotentialConvArray = [1e8, 1e8, 1e8, 1e-9, 0.001, 0.000001, 1000000, 1000,
@@ -20,3 +20,26 @@ function clearAngle() {
   originalUnit1 = elecPotentialInitValue1;
   originalUnit2 = elecPotentialInitValue2;
 }
+
+let option = "";
+for(let i=0;i<elecPotentialArray.length;i++){
+  if (i === parseInt(elecPotentialInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + elecPotentialArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + elecPotentialArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<elecPotentialArray.length;j++){
+  if (j === parseInt(elecPotentialInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + elecPotentialArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + elecPotentialArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
