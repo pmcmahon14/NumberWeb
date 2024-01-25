59d75f462e6c81dce5c73f80602e9c24622c488c
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-24 20:23:22 -0800

    fuel consumption finished

--- unit_conversions/Fuel Consumption/fuel_consumption.js
+++ unit_conversions/Fuel Consumption/fuel_consumption.js
@@ -1,4 +1,4 @@
-const List<String> fuelConsArray = ['Kilometer/Cubic Yard(km/yd³)', 'Kilometer/Cubic Feet(km/ft³)',
+const fuelConsArray = ['Kilometer/Cubic Yard(km/yd³)', 'Kilometer/Cubic Feet(km/ft³)',
   'Kilometer/Cubic Inch(km/in³)', 'Kilometer/Cubic Meter(km/m³)', 'Kilometer/Gallon (UK)(km/gal)',
   'Kilometer/Gallon (US)(km/gal)', 'Kilometer/Liter(km/l)', 'Kilometer/Quart (UK)(km/qt)',
   'Kilometer/Quart (US)(km/qt)', 'Meter/Cubic Yard(m/yd³)', 'Meter/Cubic Feet(m/ft³)',
@@ -10,12 +10,45 @@ const List<String> fuelConsArray = ['Kilometer/Cubic Yard(km/yd³)', 'Kilometer/
   'Nautical Mile/Cubic Meter(nmi/m³)', 'Nautical Mile/Gallon (UK)(nmi/gal)', 'Nautical Mile/Gallon (US)(nmi/gal)',
   'Nautical Mile/Liter(nmi/l)', 'Nautical Mile/Quart (UK)(nmi/qt)', 'Nautical Mile/Quart (US)(nmi/qt)'];
 
-String fuelConsInitValue1 = 'Mile/Gallon (US)(mi/gal)';
-String fuelConsInitValue2 = 'Meter/Liter(m/l)';
+const fuelConsInitValue1 = "23";
+const fuelConsInitValue2 = "15";
 
 //based on 1 mile/gallon (US)
-const List<double> fuelConsConvArray = [325.05, 12.04, 0.0069668567453, 425.14, 1.93, 1.61, 0.42514368317,
+const fuelConsConvArray = [325.05, 12.04, 0.0069668567453, 425.14, 1.93, 1.61, 0.42514368317,
   0.48318536166, 0.40233595748, 325045.67, 12038.73, 6.97, 425143.68, 1932.74, 1609.34, 425.14, 483.19,
   402.34, 201.97, 7.48, 0.004329004082, 264.17, 1.2, 1, 0.26417203728, 0.30023746424, 0.24999997358,
   175.51, 6.5, 0.0037618016983, 229.56, 1.04, 0.8689762419, 0.22955922417, 0.26089922336,
   0.21724403752];
+
+
+window.convArray = fuelConsConvArray;
+
+function clearAngle() {
+  selectedUnit1.value = fuelConsInitValue1;
+  selectedUnit2.value = fuelConsInitValue2;
+  originalUnit1 = fuelConsInitValue1;
+  originalUnit2 = fuelConsInitValue2;
+}
+
+let option = "";
+for(let i=0;i<fuelConsArray.length;i++){
+  if (i === parseInt(fuelConsInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + fuelConsArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + fuelConsArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<fuelConsArray.length;j++){
+  if (j === parseInt(fuelConsInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + fuelConsArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + fuelConsArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
