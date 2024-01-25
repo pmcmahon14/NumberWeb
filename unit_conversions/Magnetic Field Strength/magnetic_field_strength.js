4f7dafb88295477c030a329188374a855090f448
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-25 12:19:11 -0800

    magnetic field strength finished

---
+++ unit_conversions/Magnetic Field Strength/magnetic_field_strength.js
@@ -0,0 +1,42 @@
+const magFieldStrArray = ['Ampere/Meter (A/m)', 'Ampere/Yard (A/m)', 'Ampere/Foot (A/m)',
+  'Ampere/Inch (A/m)', 'Kiloampere/Meter (kA/m)', 'Kiloampere/Yard (A/m)', 'Kiloampere/Foot (A/m)',
+  'Kiloampere/Inch (A/m)', 'Oersted (Oe)'];
+
+const magFieldStrInitValue1 = "0";
+const magFieldStrInitValue2 = "2";
+
+//based on 1 ampere/meter
+const magFieldStrConvArray = [1, 0.9144, 0.3048, 0.0254, 0.001, 0.0009144, 0.0003048, 0.0000254,
+  0.01256637];
+
+window.convArray = magFieldStrConvArray;
+
+function clearAngle() {
+  selectedUnit1.value = magFieldStrInitValue1;
+  selectedUnit2.value = magFieldStrInitValue2;
+  originalUnit1 = magFieldStrInitValue1;
+  originalUnit2 = magFieldStrInitValue2;
+}
+
+let option = "";
+for(let i=0;i<magFieldStrArray.length;i++){
+  if (i === parseInt(magFieldStrInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + magFieldStrArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + magFieldStrArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<magFieldStrArray.length;j++){
+  if (j === parseInt(magFieldStrInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + magFieldStrArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + magFieldStrArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
