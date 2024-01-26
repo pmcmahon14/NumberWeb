4d996833557ae08e768b85883a27aefa3f6bddbb
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-26 12:23:41 -0800

    permeability finished

---
+++ unit_conversions/Permeability/permeability.js
@@ -0,0 +1,40 @@
+const permArray = ['Kilogram/Pascal/Sec/Meter² (kg/pa/s/m²)', 'Permeability (0°C) (perm)',
+  'Permeability (23°C) (perm)', 'Permeability Inches (0°C) (perm in)', 'Permeability Inches (23°C) (perm in)'];
+
+const permInitValue1 = "0";
+const permInitValue2 = "1";
+
+//based on 1 kilogram/pascal/second/Sq meter
+const permConvArray = [1, 1.747839234e10, 1.740568296e10, 6.881270558e11, 6.85264752e11];
+
+window.convArray = permConvArray;
+
+function clearAngle() {
+  selectedUnit1.value = permInitValue1;
+  selectedUnit2.value = permInitValue2;
+  originalUnit1 = permInitValue1;
+  originalUnit2 = permInitValue2;
+}
+
+let option = "";
+for(let i=0;i<permArray.length;i++){
+  if (i === parseInt(permInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + permArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + permArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<permArray.length;j++){
+  if (j === parseInt(permInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + permArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + permArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
--- unit_conversions/permeability.dart
+++
@@ -1,8 +0,0 @@
-const permArray = ['Kilogram/Pascal/Sec/Meter² (kg/pa/s/m²)', 'Permeability (0°C) (perm)',
-  'Permeability (23°C) (perm)', 'Permeability Inches (0°C) (perm in)', 'Permeability Inches (23°C) (perm in)'];
-
-String permInitValue1 = 'Kilogram/Pascal/Sec/Meter² (kg/pa/s/m²)';
-String permInitValue2 = 'Permeability (0°C) (perm)';
-
-//based on 1 kilogram/pascal/second/Sq meter
-const List<double> permConvArray = [1, 1.747839234e10, 1.740568296e10, 6.881270558e11, 6.85264752e11];
