9d905756d652fb556f1ae86987f07e54a6a45cdb
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-27 11:10:41 -0800

    surface charge density finished

--- unit_conversions/Surface Charge Density/surface_charge_density.js
+++ unit_conversions/Surface Charge Density/surface_charge_density.js
@@ -1,4 +1,4 @@
-const List<String> surfChDenArray = ['Abcoulomb/Square Meter (abC/m²)', 'Abcoulomb/Square Centimeter (abC/cm²)',
+const surfChDenArray = ['Abcoulomb/Square Meter (abC/m²)', 'Abcoulomb/Square Centimeter (abC/cm²)',
   'Abcoulomb/Square Millimeter (abC/mm²)', 'Abcoulomb/Square Micrometer (abC/µm²)',
   'Abcoulomb/Square Yard (abC/yd²)', 'Abcoulomb/Square Foot (abC/ft²)', 'Abcoulomb/Square Inch (abC/in²)',
   'Ampere Hour/Square Meter (A·h/m²)', 'Ampere Hour/Square Centimeter (A·h/cm²)',
@@ -42,11 +42,11 @@ const List<String> surfChDenArray = ['Abcoulomb/Square Meter (abC/m²)', 'Abcoul
   'Statcoulomb /Square Millimeter (stC/mm²)', 'Statcoulomb /Square Micrometer (stC/µm²)',
   'Statcoulomb /Square Yard (stC/yd²)', 'Statcoulomb /Square Foot (stC/ft²)', 'Statcoulomb /Square Inch (stC/in²)'];
 
-String surfChDenInitValue1 = 'Coulomb/Square Meter (C/m²)';
-String surfChDenInitValue2 = 'Ampere Hour/Square Meter (A·h/m²)';
+const surfChDenInitValue1 = "28";
+const surfChDenInitValue2 = "7";
 
 //based on 1 coulomb/square meter
-const List<double> surfChDenConvArray = [0.1, 0.00001, 1e-7, 1e-13, 0.083612736, 0.009290304, 0.000064516,
+const surfChDenConvArray = [0.1, 0.00001, 1e-7, 1e-13, 0.083612736, 0.009290304, 0.000064516,
   0.0002777777778, 2.777777778e-8, 2.777777778e-10, 2.777777778e-16, 0.0002322576, 0.0000258064, 1.792111111e-7,
   0.01666666667, 0.000001666666667, 1.666666667e-8, 1.666666667e-14, 0.013935456, 0.001548384, 0.00001075266667,
   1, 0.0001, 0.000001, 1e-12, 0.83612736, 0.09290304, 0.00064516, 1, 0.0001, 0.000001, 1e-12, 0.83612736,
@@ -57,3 +57,35 @@ const List<double> surfChDenConvArray = [0.1, 0.00001, 1e-7, 1e-13, 0.083612736,
   1e-9, 1e-15, 0.00083612736, 0.00009290304, 6.4516e-7, 0.000001, 1e-10, 1e-12, 1e-18, 8.3612736e-7, 9.290304e-8,
   6.4516e-10, 1000000, 100, 1, 0.000001, 836127.36, 92903.04, 645.16, 1000, 0.1, 0.001, 1e-9, 836.12736, 92.90304,
   0.64516, 2994011976, 299401.1976, 2994.011976, 0.002994011976, 2503375329, 278152814.4, 1931616.766];
+
+window.convArray = surfChDenConvArray;
+
+function clearAngle() {
+  selectedUnit1.value = surfChDenInitValue1;
+  selectedUnit2.value = surfChDenInitValue2;
+  originalUnit1 = surfChDenInitValue1;
+  originalUnit2 = surfChDenInitValue2;
+}
+
+let option = "";
+for(let i=0;i<surfChDenArray.length;i++){
+  if (i === parseInt(surfChDenInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + surfChDenArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + surfChDenArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<surfChDenArray.length;j++){
+  if (j === parseInt(surfChDenInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + surfChDenArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + surfChDenArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
