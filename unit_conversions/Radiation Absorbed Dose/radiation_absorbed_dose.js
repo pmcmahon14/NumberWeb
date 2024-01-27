0f2285c2bd4fd8a55abd03511975b898c324140b
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-27 10:35:15 -0800

    radiation absorbed dose finished

---
+++ unit_conversions/Radiation Absorbed Dose/radiation_absorbed_dose.js
@@ -0,0 +1,46 @@
+const radAbsorbedDoseArray = ['Yottagray (YGy)', 'Zettagray (ZGy)', 'Exagray (EGy)',
+  'Petagray (PGy)', 'Teragray (TGy)', 'Gigagray (GGy)', 'Megagray (MGy)', 'Kilogray (kGy)', 'Hectogray (hGy)',
+  'Decagray (daGy)', 'Gray (Gy)', 'Decigray (dGy)', 'Centigray (cGy)', 'Milligray (mGy)', 'Microgray (µGy)',
+  'Nanogray (nGy)', 'Picogray (pGy)', 'Femtogray (fGy)', 'Attogray (aGy)', 'Zeptogray (zGy)', 'Yoctogray (yGy)',
+  'Rad (rd)', 'Millirad (mrd)', 'Microrad (µrd)', 'Joule/Kilogram (J/kg)', 'Joule/Gram (J/g)',
+  'Joule/Centigram (J/cg)', 'Joule/Milligram (J/mg)', 'Sievert (Sv)', 'Millisievert (mSv)', 'Microsievert (µSv)'];
+
+const radAbsorbedDoseInitValue1 = "21";
+const radAbsorbedDoseInitValue2 = "10";
+
+//based on rad
+const radAbsorbedDoseConvArray = [1e-26, 1e-23, 1e-20, 1e-17, 1e-14, 1e-11, 1e-8, 0.00001, 0.0001,
+  0.001, 0.01, 0.1, 1, 10, 10000, 1e7, 1e10, 1e13, 1e16, 1e19, 1e22, 1, 1000, 1e6, 0.01, 0.00001, 1e-7, 1e-8,
+  0.01, 10, 10000];
+
+window.convArray = radAbsorbedDoseConvArray;
+
+function clearAngle() {
+  selectedUnit1.value = radAbsorbedDoseInitValue1;
+  selectedUnit2.value = radAbsorbedDoseInitValue2;
+  originalUnit1 = radAbsorbedDoseInitValue1;
+  originalUnit2 = radAbsorbedDoseInitValue2;
+}
+
+let option = "";
+for(let i=0;i<radAbsorbedDoseArray.length;i++){
+  if (i === parseInt(radAbsorbedDoseInitValue1)) {
+    option +=
+        '<option value="'+ i + '" selected>' + radAbsorbedDoseArray[i] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ i + '">' + radAbsorbedDoseArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option;
+
+for(let j=0;j<radAbsorbedDoseArray.length;j++){
+  if (j === parseInt(radAbsorbedDoseInitValue2)) {
+    option +=
+        '<option value="'+ j + '" selected>' + radAbsorbedDoseArray[j] + "</option>"
+  } else {
+    option +=
+        '<option value="'+ j + '">' + radAbsorbedDoseArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option;
