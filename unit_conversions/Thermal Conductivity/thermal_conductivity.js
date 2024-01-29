ce0570e21a8bdf6bfe5af15651ba94752c6427a5
pmcmahon14 <pmcmahon14@gmail.com>
2024-01-28 23:08:37 -0800

    thermal conductivity finished

--- unit_conversions/Thermal Conductivity/thermal_conductivity.js
+++ unit_conversions/Thermal Conductivity/thermal_conductivity.js
@@ -1,4 +1,4 @@
-const List<String> thermCondArray = ['BTU Inch (IT)/Second/Square Foot/Fahrenheit\n (BTU·in·s⁻¹·ft2·°F⁻¹)',
+const thermCondArray = ['BTU Inch (IT)/Second/Square Foot/Fahrenheit\n (BTU·in·s⁻¹·ft2·°F⁻¹)',
   'BTU Inch (TH)/Second/Square Foot/Fahrenheit\n (BTU·in·s⁻¹·ft2·°F⁻¹)',
   'BTU Foot (IT)/Second/Square Foot/Fahrenheit\n (BTU·ft·s⁻¹·ft2·°F⁻¹)',
   'BTU Foot (TH)/Second/Square Foot/Fahrenheit\n (BTU·ft·s⁻¹·ft2·°F⁻¹)',
@@ -25,11 +25,44 @@ const List<String> thermCondArray = ['BTU Inch (IT)/Second/Square Foot/Fahrenhei
   'Kilowatt/Meter/Celsius (kW·m⁻¹·K)', 'Kilowatt/Centimeter/Celsius (kW·cm⁻¹·K)',
   'Watt/Meter/Celsius (W·m⁻¹·K⁻¹)', 'Watt/Centimeter/Celsius (W·cm⁻¹·°K⁻¹)'];
 
-String thermCondInitValue1 = 'Watt/Meter/Celsius (W·m⁻¹·K⁻¹)';
-String thermCondInitValue2 = 'Watt/Centimeter/Celsius (W·cm⁻¹·°K⁻¹)';
+const thermCondInitValue1 = "26";
+const thermCondInitValue2 = "27";
 
 //based on 1 watt/meter/Celsius
-const List<double> thermCondConvArray = [0.001925964388, 0.00192725321, 0.0001604970324, 0.0001606044342, 6.93,
+const thermCondConvArray = [0.001925964388, 0.00192725321, 0.0001604970324, 0.0001606044342, 6.93,
   6.94, 0.5777893165, 0.578175963, 0.2388458966, 0.2390057361, 0.002388458966, 0.002390057361, 859.85, 860.42,
   8.5985, 8.6042, 0.0002388458966, 0.0002390057361, 2.388458966e-6, 2.390057361e-6, 0.8598452279, 0.8604206501,
   0.008598452279, 0.008604206501, 1, 0.008604206501, 0.001, 0.00001];
+
+window.convArray = thermCondConvArray;
+
+function clearAngle() {
+  selectedUnit1.value = thermCondInitValue1;
+  selectedUnit2.value = thermCondInitValue2;
+  originalUnit1 = thermCondInitValue1;
+  originalUnit2 = thermCondInitValue2;
+}
+
+let option1 = "";
+for(let i=0;i<thermCondArray.length;i++){
+  if (i === parseInt(thermCondInitValue1)) {
+    option1 +=
+        '<option value="'+ i + '" selected>' + thermCondArray[i] + "</option>"
+  } else {
+    option1 +=
+        '<option value="'+ i + '">' + thermCondArray[i] + "</option>"
+  }
+}
+document.getElementById("unit1").innerHTML = option1;
+
+let option2 = "";
+for(let j=0;j<thermCondArray.length;j++){
+  if (j === parseInt(thermCondInitValue2)) {
+    option2 +=
+        '<option value="'+ j + '" selected>' + thermCondArray[j] + "</option>"
+  } else {
+    option2 +=
+        '<option value="'+ j + '">' + thermCondArray[j] + "</option>"
+  }
+}
+document.getElementById("unit2").innerHTML = option2;
