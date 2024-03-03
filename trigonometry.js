39ca97a4bbb149ed9a3783d6b5a5e9522d2f36eb
pmcmahon14 <pmcmahon14@gmail.com>
2024-03-03 12:38:46 -0800

    trig calculator finished with 24 calculations

--- trigonometry.js
+++ trigonometry.js
@@ -13,6 +13,9 @@ let acosButton = document.getElementById("acos");
 let atanButton = document.getElementById("atan");
 let shiftDisplay = document.getElementById("shiftDisp");
 let hypDisplay = document.getElementById("hypDisp");
+
+//following two are hidden from view on webpage; meant to align buttons
+//and help label trig buttons correctly and are just below forms
 let shiftValue = shiftDisplay.innerHTML;
 let hypValue = hypDisplay.innerHTML;
 
@@ -20,17 +23,20 @@ let decimalUsed = false;
 
 const radianDivide = 1;
 const degreeDivide = (Math.PI/180);
-const altDegreeDivide = (180/Math.PI);
 const gradianDivide = ((Math.PI/180)*(1/9)*10);
+const altDegreeDivide = (180/Math.PI);
+const altGradianDivide = (180/Math.PI)*((1/9)*10);
 let measure = degreeDivide;
 let altMeasure = altDegreeDivide;
 
 const sinArray = ["sin", "cos", "tan"];
-const secArray = ["sec", "csc", "cot"];
+const secArray = ["csc", "sec", "cot"];
 const asinArray = ["asin", "acos", "atan"];
+const acscArray = ["acsc", "asec", "acot"];
 const sinhArray = ["sinh", "cosh", "tanh"];
-const sechArray = ["sech", "csch", "coth"];
+const sechArray = ["csch", "sech", "coth"];
 const asinhArray = ["asinh", "acosh", "atanh"];
+const acschArray = ["acsch", "asech", "acoth"];
 let bottomArray = sinArray;
 let topArray = asinArray;
 sinButton.innerHTML = bottomArray[0];
@@ -64,13 +70,13 @@ function shiftHypSelectors() {
         topArray = asinArray;
     } else if (shiftValue === "SHIFT" && hypValue === "") {
         bottomArray = secArray;
-        topArray = asinArray;
+        topArray = acscArray;
     } else if (shiftValue === "" && hypValue === "HYP") {
         bottomArray = sinhArray;
         topArray = asinhArray;
     } else {
         bottomArray = sechArray;
-        topArray = asinhArray;
+        topArray = acschArray;
     }
     sinButton.innerHTML = bottomArray[0];
     cosButton.innerHTML = bottomArray[1];
@@ -92,6 +98,7 @@ function appendToDisplay(input) {
         //nothing
     } else if (inputDisplay.value === "0") {
         inputDisplay.value = input;
+        calculate();
     } else {
         inputDisplay.value = inputDisplay.value+input;
         calculate();
@@ -102,12 +109,15 @@ function selector() {
     if (document.getElementById("select").innerHTML === "DEGREE") {
         document.getElementById("select").innerHTML = "RADIAN";
         measure = radianDivide;
+        altMeasure = radianDivide;
     } else if (document.getElementById("select").innerHTML === "RADIAN") {
         document.getElementById("select").innerHTML = "GRADIAN";
         measure = gradianDivide;
+        altMeasure = altGradianDivide;
     } else {
         document.getElementById("select").innerHTML = "DEGREE";
         measure = degreeDivide;
+        altMeasure = altDegreeDivide;
     }
     calculate();
 }
@@ -119,6 +129,9 @@ function clearAll() {
     outputDisplay.value = '0';
     outputLabel.innerText = "sin";
     decimalUsed = false;
+    shiftValue = "";
+    hypValue = "";
+    shiftHypSelectors();
 }
 
 function clearError() {
@@ -126,6 +139,8 @@ function clearError() {
     // tempTrig = Math.floor(tempTrig/10);
     if (inputDisplay.value.slice(".")) {
         decimalUsed = false;
+    } else if (inputDisplay.value === "-") {
+        inputDisplay.value = "0";
     }
     let tempTrig = inputDisplay.value.substring(0, inputDisplay.value.length-1);
     if(tempTrig === ""){
@@ -145,8 +160,12 @@ function decimalCalc() {
 }
 
 function plusMinus() {
-    if (inputDisplay.value.startsWith("-")) {
+    if (inputDisplay.value === "-") {
+        inputDisplay.value = "0";
+    } else if (inputDisplay.value.startsWith("-")) {
         inputDisplay.value = inputDisplay.value.substring(1);
+    } else if (inputDisplay.value === "0") {
+        inputDisplay.value = "-";
     } else {
         inputDisplay.value = "-"+inputDisplay.value;
     }
@@ -168,16 +187,16 @@ function calculate() {
             outputDisplay.value = Math.asin(inputDisplay.value)*altMeasure.toFixed(10);
             break;
         case "acos":
-            outputDisplay.value = Math.acos(inputDisplay.value*altMeasure).toFixed(10);
+            outputDisplay.value = Math.acos(inputDisplay.value)*altMeasure.toFixed(10);
             break;
         case "atan":
-            outputDisplay.value = Math.atan(inputDisplay.value*altMeasure).toFixed(10);
+            outputDisplay.value = Math.atan(inputDisplay.value)*altMeasure.toFixed(10);
             break;
         case "sec":
-            outputDisplay.value = (1/(Math.sin(inputDisplay.value*measure))).toFixed(10);
+            outputDisplay.value = (1/(Math.cos(inputDisplay.value*measure))).toFixed(10);
             break;
         case "csc":
-            outputDisplay.value = (1/(Math.cos(inputDisplay.value*measure))).toFixed(10);
+            outputDisplay.value = (1/(Math.sin(inputDisplay.value*measure))).toFixed(10);
             break;
         case "cot":
             outputDisplay.value = (1/(Math.tan(inputDisplay.value*measure))).toFixed(10);
@@ -192,13 +211,13 @@ function calculate() {
             outputDisplay.value = Math.tanh(inputDisplay.value*measure).toFixed(10);
             break;
         case "asinh":
-            outputDisplay.value = Math.asinh(inputDisplay.value*altMeasure).toFixed(10);
+            outputDisplay.value = Math.asinh(inputDisplay.value)*altMeasure.toFixed(10);
             break;
         case "acosh":
-            outputDisplay.value = Math.acosh(inputDisplay.value*altMeasure).toFixed(10);
+            outputDisplay.value = Math.acosh(inputDisplay.value)*altMeasure.toFixed(10);
             break;
         case "atanh":
-            outputDisplay.value = Math.atanh(inputDisplay.value*altMeasure).toFixed(10);
+            outputDisplay.value = Math.atanh(inputDisplay.value)*altMeasure.toFixed(10);
             break;
         case "sech":
             outputDisplay.value = (1/(Math.sinh(inputDisplay.value*measure))).toFixed(10);
@@ -209,9 +228,37 @@ function calculate() {
         case "coth":
             outputDisplay.value = (1/(Math.tanh(inputDisplay.value*measure))).toFixed(10);
             break;
-
-            //todo add acsch, asech, acoth
         case "acsch":
-            outputDisplay.value = (Math.log(Math.sqrt(1+(1/(45*deg)))+(1/(45*deg)))).toFixed(10);
+            const p1 = Math.sqrt(1+Math.pow(inputDisplay.value,2));
+            const p2 = (1+p1)/inputDisplay.value;
+            const p3 = Math.log(p2)*altMeasure;
+            outputDisplay.value = p3.toFixed(10);
+            break;
+        case "asech":
+            const p4 = Math.sqrt((1/Math.pow(inputDisplay.value,2))-1);
+            const p5 = 1/inputDisplay.value;
+            const p6 = Math.log(p5+p4)*altMeasure;
+            outputDisplay.value = p6.toFixed(10);
+            break;
+        case "acoth":
+            const p7 = Math.log(1-(1/inputDisplay.value));
+            const p8 = Math.log(1+(1/inputDisplay.value));
+            const p9 = ((1/2)*(p8-p7))*altMeasure;
+            outputDisplay.value = p9.toFixed(10);
+            break;
+        case "acsc":
+            outputDisplay.value = (Math.asin(1/inputDisplay.value)*altMeasure).toFixed(10);
+            break;
+        case "asec":
+            outputDisplay.value = (Math.acos(1/inputDisplay.value)*altMeasure).toFixed(10);
+            break;
+        case "acot":
+            outputDisplay.value = (Math.atan(1/inputDisplay.value)*altMeasure).toFixed(10);
+            break;
+    }
+    if (outputDisplay.value.endsWith("0000000000")) {
+        outputDisplay.value = Math.round(outputDisplay.value);
+    } else if (outputDisplay.value === "NaN") {
+        outputDisplay.value = "error";
     }
 }
